4907f234d7aed3bc033c56e7afea46a47fd92a51
hecencelb <hencinas23@gmail.com>
2024-11-07 13:49:49 +0100

    todo hecho

--- src/ej30.java
+++ src/ej30.java
@@ -6,16 +6,40 @@ public class ej30 {
         double i = 1;
         double num1;
         double num2;
-        while (i == -222) {
-            double signo;
+        while (i == 1) {
+            String signo;
             System.out.println("Escribe el numero: ");
             num1 = scan.nextDouble();
             System.out.println("Escribe el signo: ");
-            signo = scan.nextDouble();
+            signo = scan.next();
             System.out.println("Escribe el otro numero: ");
             num2 = scan.nextDouble();
+            switch(signo){
+                case "-":
+                double resultado= num1 - num2;
+                System.out.println(resultado);
+                break;
+                case "+":
+                    resultado= num1 + num2;
+                    System.out.println(resultado);
+                    break;
+                case "*":
+                    resultado= num1 * num2;
+                    System.out.println(resultado);
+                    break;
+                case "/":
+                    resultado= num1 / num2;
+                    System.out.println(resultado);
+                    break;
+                case "%":
+                    resultado= num1 % num2;
+                    System.out.println(resultado);
+                    break;
 
-            System.out.println("si desea cerrar el programa escriba -222 si no escriba cualquier otro numero para empez ");
+            }
+            System.out.println("si desea cerrar el programa escriba -222 si no escriba cualquier otro numero para continuar calculando ");
+            i= scan.nextDouble();
         }
     }
 }
+
