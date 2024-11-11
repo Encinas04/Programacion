30d5a350e54a4c9e38c8247bc58d5835fa58fd23
hecencelb <hencinas23@gmail.com>
2024-11-11 10:00:56 +0100

    todo hecho

--- src/ej25.java
+++ src/ej25.java
@@ -3,15 +3,18 @@ public class ej25 {
     public static void main(String[]args){
         Scanner scan=new Scanner(System.in);
         int numero=0;
-        int numero1=1;
+        //int numero1=1;
         int conteo=1;
         System.out.println("inserte el numero:");
         numero= scan.nextInt();
-        while(numero>=numero1){
+        for (int numero1=1;numero>=numero1;numero1++){
+            conteo=conteo*numero1;
+        }
+        /*while(numero>=numero1){
             conteo=conteo*numero1;
             numero1++;
 
-        }
+        }*/
         System.out.println("El numero factorial es:"+ conteo);
     }
 }
